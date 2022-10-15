032db2be349ba1029cd1db70ce0b509eabc03adb
ignacio grilli <grillidavidignacio@gmail.com>
2022-10-15 19:27:49 -0300

    copia rama de eze con cambios transacoines del usuario y eso

--- src/main/java/com/desarrollo/sem/service/TransaccionesCCServive.java
+++ src/main/java/com/desarrollo/sem/service/TransaccionesCCServive.java
@@ -1,14 +1,25 @@
 package com.desarrollo.sem.service;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 //import com.desarrollo.sem.model.CuentaCorriente;
 import com.desarrollo.sem.model.TransaccionesCC;
 
 @Repository
-public interface TransaccionesCCServive extends JpaRepository<TransaccionesCC, Long>  {
+public interface TransaccionesCCServive extends JpaRepository<TransaccionesCC, Long> {
 
-//    TransaccionesCC save(TransaccionesCC transaccionesCC, Optional<CuentaCorriente> cuenta);
+  // TransaccionesCC save(TransaccionesCC transaccionesCC,
+  // Optional<CuentaCorriente> cuenta);
+
+  @Query(value = "SELECT p from TransaccionesCC p")
+  List<TransaccionesCC> find();
+
+  @Query(value = "SELECT p FROM TransaccionesCC p WHERE p.cuenta.mail = ?1")
+  List<TransaccionesCC> getTransaccionesUsuario(String num2);
+
 
 }
