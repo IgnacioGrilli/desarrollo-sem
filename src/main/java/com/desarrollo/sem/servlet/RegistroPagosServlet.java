886c233b619e4f39d316dbe5e6015d11ccc25175
Ezequiel <ezequielalb10@gmail.com>
2024-05-09 01:31:20 -0300

    fecha arreglada

--- src/main/java/com/desarrollo/sem/servlet/RegistroPagosServlet.java
+++ src/main/java/com/desarrollo/sem/servlet/RegistroPagosServlet.java
@@ -79,24 +79,28 @@ public class RegistroPagosServlet {
    
 
         RegistroPagosDiarios reg = service.findOneByUuid(UUID.fromString(id));/* .orElseThrow(); */
+        System.out.println("1+" + reg.getFecha().getTime());
         
        /*  if (registro.getHoraFin().getTime() < reg.getHoraInicio().getTime())
             return throw new Exception("La hora de fin no puede ser menor a la hora de inicio");
          */
         reg.setHoraFin(registro.getHoraFin());
- 
+        System.out.println("2+" + reg.getFecha().getTime());
         //---- valida que la hora de finalizacion este en el horario vigente
         /* System.out.println(reg.getHoraInicio().getTime()/3600000);
         System.out.println(registro.getHoraFin().getTime()-10800000); */ 
         //System.out.println(reg.getFecha().toString());
         Calendar cal = reg.getFecha();
+        System.out.println("asdada+" + reg.getFecha().getTime());
         cal.set(Calendar.YEAR, 2022);
         cal.set(Calendar.MONTH, 10);
         cal.set(Calendar.DATE, 10);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.MILLISECOND, 0);
+        System.out.println("aca+" + reg.getFecha().getTime());
         cal.setTime(registro.getHoraFin());
-/* 
+        System.out.println("3+" + reg.getFecha().getTime());
+/*      
         Calendar cal2 = reg.getFecha();
         cal2.set(Calendar.YEAR, 2022);
         cal2.set(Calendar.MONTH, 10);
@@ -115,7 +119,7 @@ public class RegistroPagosServlet {
             reg.setHoraFin(cal.getTime());
         }
             
-         
+        System.out.println("4+" + reg.getFecha().getTime());
         //si el estacionamiento se finaliza luego del horario de la tarde
         if ((registro.getHoraFin().getTime()-10800000)/3600000>=valService.valorActual().getHsFinT()
         && ((reg.getHoraInicio().getTime()-10800000)/3600000>=valService.valorActual().getHsInicioT())) {
@@ -131,14 +135,15 @@ public class RegistroPagosServlet {
         //System.out.println(registro.getHoraFin().toString());
         
        //System.out.println(reg.getHoraFin().get);
-       
+       System.out.println("5+" + reg.getFecha().getTime());
         int min = ((reg.getHoraFin().getHours()*60+reg.getHoraFin().getMinutes()) - (reg.getHoraInicio().getHours()*60+reg.getHoraInicio().getMinutes()));
         //Long min = (reg.getHoraInicio().getTime() - reg.getHoraFin().getTime()) / 60000;
         System.out.println(min);
         int val = (int) Math.round(min * valService.valorActual().getValor());
         reg.setValor(val);
         descSaldo(-val, reg.getConductor());
-        
+        reg.setFecha(registro.getFecha());
+        System.out.println("final" + reg.getFecha().getTime());
         return service.save(reg);
     }
 
