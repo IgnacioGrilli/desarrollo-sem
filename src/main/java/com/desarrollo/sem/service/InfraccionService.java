97b3e0fe8d06fb1d9034498a934f7b6b13d7656c
davdenis <denis13dav@gmail.com>
2024-06-17 18:30:08 -0300

    Servicio infracciones por fecha

--- src/main/java/com/desarrollo/sem/service/InfraccionService.java
+++ src/main/java/com/desarrollo/sem/service/InfraccionService.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +22,18 @@ interface InfraccionRepository extends JpaRepository<InfraccionDTO, Long> {
 
     @Query(value = "SELECT uo.id AS obleistaId, uo.nombre AS obleistaNombre, SUM(rpd.valor) AS totalValor, COUNT(*) AS totalRegistros FROM public.registro_pagos_diarios rpd JOIN public.usuario_obleista uo ON rpd.obleista_id = uo.id GROUP BY uo.id, uo.nombre", nativeQuery = true)
     List<ObleistaSummaryProjection> findObleistaSummaries();
+
+    // Nueva consulta para obtener infracciones por fecha específica
+    @Query(value = "SELECT rpd.uuid AS id, rpd.fecha AS fecha, rpd.patenteid AS patente, rpd.hora AS hora " +
+            "FROM registro_patentes_diarios rpd " +
+            "WHERE rpd.fecha = ?1 " +
+            "EXCEPT " +
+            "SELECT DISTINCT ON (rpat.uuid) rpat.uuid, rpat.fecha, rpat.patenteid, rpat.hora " +
+            "FROM registro_pagos_diarios rpag " +
+            "JOIN registro_patentes_diarios rpat ON rpag.patente_id = rpat.patenteid " +
+            "WHERE rpag.fecha = ?1 AND rpat.hora >= rpag.hora_inicio AND rpat.hora <= rpag.hora_fin",
+            nativeQuery = true)
+    List<InfraccionDTO> findInfraccionesByFecha(Date fecha);
 }
 
 @Service
@@ -33,12 +46,12 @@ public class InfraccionService {
         return infraccionRepository.findInfraccionesByPatente(patente);
     }
 
-    // Nuevo método para obtener todas las infracciones sin distinción de patentes
+    //  método para obtener todas las infracciones sin distinción de patentes
     public List<InfraccionDTO> getAllInfracciones() {
         return infraccionRepository.findAllInfracciones();
     }
 
-    // Nuevo método para obtener el resumen de los obleistas
+    //  método para obtener el resumen de los obleistas
     public List<ObleistaSummaryDTO> getObleistaSummaries() {
         List<ObleistaSummaryProjection> projections = infraccionRepository.findObleistaSummaries();
         return projections.stream()
@@ -53,6 +66,11 @@ public class InfraccionService {
         dto.setTotalValor(projection.getTotalValor());
         dto.setTotalRegistros(projection.getTotalRegistros());
         return dto;
+    }
+
+    // Método para obtener infracciones por fecha
+    public List<InfraccionDTO> getInfraccionesByFecha(Date fecha) {
+        return infraccionRepository.findInfraccionesByFecha(fecha);
     }
 
 
